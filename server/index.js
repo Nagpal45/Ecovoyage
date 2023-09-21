f9004148d93f3991fec0c911a3a0f7d3119efbdc
Nagpal45 <nagpal4503@gmail.com>
2023-09-21 17:53:52 +0530

    login and registration working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
@@ -12,6 +15,7 @@ mongoose.connect(process.env.MONGO_DB, {
     useUnifiedTopology: true,
 }).then(console.log(`MongoDB Connected`)).catch(err => console.log(err))
 
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
