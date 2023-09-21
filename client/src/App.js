83147b58eae7e3a87e27c9b3b2b60c57d571b790
suvigyagarg <suvigya.2003.garg@gmail.com>
2023-09-21 20:57:07 +0530

    navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './components/footer/footer';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>React App</h1>
+    <Footer/>
     </div>
   );
 }
