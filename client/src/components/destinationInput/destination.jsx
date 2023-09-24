d8077e35d80380ea8f92e8806e98d954a0897d10
Nagpal45 <nagpal4503@gmail.com>
2023-09-24 09:37:34 +0530

    search bar changed

--- client/src/components/destinationInput/destination.jsx
+++ client/src/components/destinationInput/destination.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./destination.css";
-import TextField from "@material-ui/core/TextField";
 import {
   Category,
   DirectionsBike,
@@ -8,83 +7,80 @@ import {
   Home,
   Search,
 } from "@material-ui/icons";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import Button from "@material-ui/core/Button";
 import Typewriter from "typewriter-effect";
-import { makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-  customTextField: {
-    width: "65%",
-
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "black",
-        borderWidth: "0.2vw",
-        borderRadius: "2vw",
-        margin: "auto",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "gray",
-        borderWidth: "0.2vw",
-      },
-    },
-  },
-  selectedOption: {
-    borderBottom: "0.3vw solid #000",
-  },
-  recommendationList: {
-    position: "absolute",
-    top:"5.5vw",
-    width: "60%",
-    zIndex: 1,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-
-  },
-  recommendationItem: {
-    padding: "0.5rem",
-    cursor: "pointer",
-  },
-}));
 
 export default function Destination() {
   const [activeOption, setActiveOption] = useState("all");
-  const [searchText, setSearchText] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const [recommendations, setRecommendations] = useState([]);
 
-  const recommendationData = {
-    all: ["Recommendation 1", "Recommendation 2", "Recommendation 3"],
-    accommodation: ["Accommodation 1", "Accommodation 2", "Accommodation 3"],
-    transportation: [
-      "Transportation 1",
-      "Transportation 2",
-      "Transportation 3",
-    ],
-    activities: ["Activity 1", "Activity 2", "Activity 3"],
+  const [isInputFocused, setInputFocus] = useState(false);
+
+  const inputRef = useRef(null);
+  // Handle focus and blur events of the input field
+  const handleInputFocus = () => {
+    setInputFocus(true);
   };
+  const handleInputBlur = () => {
+    setInputFocus(false);
+  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (inputRef.current && !inputRef.current.contains(event.target)) {
+        setInputFocus(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
 
   const handleOptionClick = (option) => {
     setActiveOption(option);
-    setSearchText("");
-    setRecommendations([]);
+    const newRecommendations = generateRecommendations(option, searchQuery);
+    setRecommendations(newRecommendations);
   };
 
   const handleInputChange = (event) => {
-    const inputText = event.target.value;
-    setSearchText(inputText);
+    const query = event.target.value;
+    setSearchQuery(query);
 
-    const filteredRecommendations = recommendationData[activeOption]
-      .filter((recommendation) =>
-        recommendation.toLowerCase().includes(inputText.toLowerCase())
-      )
-      .slice(0, 5);
-
-    setRecommendations(filteredRecommendations);
+    if (event.target === document.activeElement) {
+      const newRecommendations = generateRecommendations(activeOption, query);
+      setRecommendations(newRecommendations);
+    } else {
+      setRecommendations([]);
+    }
   };
 
-  const classes = useStyles();
+  function generateRecommendations(selectedOption, query) {
+    const dummyData = {
+      all: ["Hotel in New York", "Hiking in the Alps", "Car rental deals"],
+      accommodation: [
+        "Hotels in Paris",
+        "Luxury resorts in Bali",
+        "Cabin rentals in the Rockies",
+      ],
+      activities: [
+        "Biking trails near you",
+        "Scuba diving spots",
+        "Ski resorts in Switzerland",
+      ],
+      transportation: [
+        "Flight to Tokyo",
+        "Train tickets in Europe",
+        "Rental cars in Miami",
+      ],
+    };
+
+    const matchingRecommendations = dummyData[selectedOption].filter((item) =>
+      item.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return matchingRecommendations || [];
+  }
   return (
     <div className="destination" id="destination">
       <div className="wrapper">
@@ -115,36 +111,40 @@ export default function Destination() {
         </div>
         <div className="options">
           <div
-            className={`option ${
-              activeOption === "all" ? classes.selectedOption : ""
-            }`}
+            className={
+              activeOption === "all" ? "activeoption option" : "option"
+            }
             onClick={() => handleOptionClick("all")}
           >
             <Category className="optionIcon" />
             <span className="optionName">Search all</span>
           </div>
           <div
-            className={`option ${
-              activeOption === "accommodation" ? classes.selectedOption : ""
-            }`}
+            className={
+              activeOption === "accommodation"
+                ? "activeoption option"
+                : "option"
+            }
             onClick={() => handleOptionClick("accommodation")}
           >
             <Home className="optionIcon" />
             <span className="optionName">Accommodation</span>
           </div>
           <div
-            className={`option ${
-              activeOption === "transportation" ? classes.selectedOption : ""
-            }`}
+            className={
+              activeOption === "transportation"
+                ? "activeoption option"
+                : "option"
+            }
             onClick={() => handleOptionClick("transportation")}
           >
             <EmojiTransportation className="optionIcon" />
             <span className="optionName">Transportation</span>
           </div>
           <div
-            className={`option ${
-              activeOption === "activities" ? classes.selectedOption : ""
-            }`}
+            className={
+              activeOption === "activities" ? "activeoption option" : "option"
+            }
             onClick={() => handleOptionClick("activities")}
           >
             <DirectionsBike className="optionIcon" />
@@ -152,56 +152,28 @@ export default function Destination() {
           </div>
         </div>
         <div className="searchBar">
-          <TextField
-            variant="outlined"
-            className={classes.customTextField}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Button
-                    variant="contained"
-                    style={{
-                      borderRadius: "1.8vw",
-                      fontSize: "0.9vw",
-                      backgroundColor: "#34e0a1",
-                      color: "#000000",
-                      fontFamily: "QuickSand",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    search
-                  </Button>
-                </InputAdornment>
-              ),
-              style: {
-                fontSize: "1.2vw",
-                fontFamily: "QuickSand",
-                fontWeight: "bold",
-                height: "3.5vw",
-              },
-            }}
-            placeholder={`Search ${activeOption ? `for ${activeOption}` : ""}`}
-            fullWidth
-            value={searchText}
-            onChange={handleInputChange}
-          />
-           {recommendations.length > 0 && (
-            <div className={classes.recommendationList}>
-              {recommendations.map((recommendation, index) => (
-                <div
-                  key={index}
-                  className={classes.recommendationItem}
-                  onClick={() => setSearchText(recommendation)}
-                >
-                  {recommendation}
-                </div>
+          <div className="searchInputContainer">
+            <Search className="search" />
+            <input
+              type="text"
+              className="searchInput"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={handleInputChange}
+              onFocus={handleInputFocus} 
+              onBlur={handleInputBlur} 
+              ref={inputRef} 
+            />
+          </div>
+          {document.activeElement ===
+            document.querySelector(".searchInput") && isInputFocused && recommendations.length > 0 && (
+            <ul className="recommendations">
+              {recommendations.map((item, index) => (
+                <li key={index} className="recommendationItem">
+                  {item}
+                </li>
               ))}
-            </div>
+            </ul>
           )}
         </div>
         <img src="/images/down.png" alt="" className="downArrow" />
