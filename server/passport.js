aeeb26f434bfd7c8f5fd3ec690f04d6f2d41e57e
Nagpal45 <nagpal4503@gmail.com>
2023-09-27 21:08:22 +0530

    google auth and sign in design

---
+++ server/passport.js
@@ -0,0 +1,41 @@
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const passport = require("passport");
+dotenv = require("dotenv");
+dotenv.config();
+const User = require("./models/User");
+const db = require("./db");
+
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        let user = await User.findOne({email: profile.email});
+
+        if (!user) {
+            user = new User({
+                username: profile.displayName,
+                email: profile.email,
+                password: profile.id,
+                profilePicture: profile.picture,
+            });
+            await user.save();
+            console.log(user);
+        }
+        return done(null, user);
+    }
+  )
+);
+
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
