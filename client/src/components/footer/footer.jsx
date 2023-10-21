a7f6daf64bf225ed14edb42f9bad53c59cfc36f0
suvigyagarg <suvigya.2003.garg@gmail.com>
2023-10-21 14:38:30 +0530

    Conflict error solved

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -18,58 +18,6 @@ const Footer = () => {
               {/* Add more navigation links as needed */}
             </ul>
           </div>
-        <section className="">
-          <div className="container text-center text-md-start mt-5">
-            <div className="row mt-3">
-              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3 text-secondary"></i>EcoVoyage
-                </h6>
-                <p>
-                Prioritizing eco-conscious travel for memorable experiences with minimal carbon emissions.
-                </p>
-              </div>
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  Services
-                </h6>
-                <p>
-                  <a href="#!" className="text-reset">Accommodation</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Transportation</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Activities</a>
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  Useful links
-                </h6>
-                <p>
-                  <a href="#!" className="text-reset">Settings</a>
-                </p>                <p>
-                  <a href="#!" className="text-reset">Help</a>
-                </p>
-              </div>
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-                <p><i className="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
-                <p>
-                  <i className="fas fa-envelope me-3 text-secondary"></i>
-                  info@example.com
-                </p>
-                <p><i className="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
-                <p><i className="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.025)' }}>
-          © 2021 
-          <a className="text-reset fw-bold" href=""> EcoVoyage</a>
           <div className="footer-social">
             <p className='social'>Connect with us:</p>
             <a className="text-body-secondary media" href="https://www.instagram.com/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
@@ -84,7 +32,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-    </div>
     </footer>
   );
 };
