ec4dd4f2a8d0469a6aeb63c56a109b7c751bc496
Nagpal45 <nagpal4503@gmail.com>
2023-11-28 20:17:03 +0530

    changed suggestions and predict wworking

--- client/src/pages/travelPlan/plan.jsx
+++ client/src/pages/travelPlan/plan.jsx
@@ -7,16 +7,11 @@ import Map from "../../components/map/map.jsx";
 
 
 export default function Plan() {
-  
   const [carMake, setCarMake] = useState("");
   const [carModel, setCarModel] = useState("");
   const [vehicleClass, setVehicleClass] = useState("");
   const [transmission, setTransmission] = useState("");
   const [fuelType, setFuelType] = useState("");
-  const [arrival, setArrival] = useState([]);
-  const [destination, setDestination] = useState([]);
-  const [arrivalSuggestions, setArrivalSuggestions] = useState([]);
-  const [destinationSuggestions, setDestinationSuggestions] = useState([]);
   const [arrivalCoordinates, setArrivalCoordinates] = useState([]);
   const [destinationCoordinates, setDestinationCoordinates] = useState([]);
   const [models, setModels] = useState([]);
@@ -26,7 +21,6 @@ export default function Plan() {
   const [predictedCO2, setPredictedCO2] = useState(null);
   const [totalDistance, setTotalDistance] = useState(null);
   const [totalCO2, setTotalCO2] = useState(null);
-  // const [hotelData, setHotelData] = useState([]);
   const [coordinates, setCoordinates] = useState(null);
   const [bounds, setBounds] = useState(null);
   const [places, setPlaces] = useState([]);
@@ -127,51 +121,18 @@ export default function Plan() {
       if (name === "vehicleClass") setVehicleClass(value);
       else if (name === "transmission") setTransmission(value);
       else if (name === "fuelType") setFuelType(value);
-      else if (name === "arrival") setArrival(value);
-      else if (name === "destination") setDestination(value);
-      handleAutocomplete(name, value);
     }
   };
 
-  const handleSelect = async (value, name) => {
-    if(name === "arrival") {
-      setArrival(value);
-      setArrivalSuggestions([]);
-      fetchCoordinates(name,value);
-    }
-    else {
-      setDestination(value);
-      setDestinationSuggestions([]);
-      fetchCoordinates(name,value);
-    }
-    console.log();
-  };
-
-  const fetchCoordinates = async (name,value) => {
-    try {
-      const response = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?q=${[value]}&key=d4e73b5ffb22404f9fd4ac67eafae80d`
-      );
- 
-      const coordinates = response.data.results[0].geometry;
-      console.log(coordinates);
-
-      if(name === "arrival") setArrivalCoordinates([coordinates.lat, coordinates.lng]);
-      else setDestinationCoordinates([coordinates.lat, coordinates.lng]);
-      
-     
-    } catch (error) {
-      console.error("Error fetching coordinates:", error);
-    }
-  };
-
+  
   const calculateDistance = async () => {
+    console.log(arrivalCoordinates, destinationCoordinates);
     if (arrivalCoordinates && destinationCoordinates) {
       const R = 6371; 
-      const lat1 = arrivalCoordinates[0];
-      const lon1 = arrivalCoordinates[1];
-      const lat2 = destinationCoordinates[0];
-      const lon2 = destinationCoordinates[1];
+      const lat1 = arrivalCoordinates.lat;
+      const lon1 = arrivalCoordinates.lng;
+      const lat2 = destinationCoordinates.lat;
+      const lon2 = destinationCoordinates.lng;
 
       const dLat = (lat2 - lat1) * (Math.PI / 180);
       const dLon = (lon2 - lon1) * (Math.PI / 180);
@@ -186,60 +147,32 @@ export default function Plan() {
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
       const distance = R * c;
-
+      console.log(distance);
      setTotalDistance(distance);
     }
   };
 
-  const handleAutocomplete = async (name, query) => {
-    try {
-      const response = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=d4e73b5ffb22404f9fd4ac67eafae80d`
-      );
-
-      const suggestions = response.data.results.map(
-        (result) => result.formatted
-      );
-      if (name === "arrival") setArrivalSuggestions(suggestions);
-      else setDestinationSuggestions(suggestions);
-    } catch (error) {
-      console.error("Error fetching autocomplete suggestions:", error);
-    }
-  };
-
+  
   const calculateCO2 = () => {
     const CO2 = (predictedCO2 * totalDistance) / 1000;
     setTotalCO2(CO2);
   };
 
-  const handlePlanSubmit = (event) => {
+  const handlePlanSubmit = async (event) => {
     event.preventDefault();
-    sendCarInfotoMLmodel();
-    setTimeout(() => {
+    await sendCarInfotoMLmodel();
+  };
+
+  useEffect(() => {
+    if (predictedCO2 && totalDistance) {
       calculateCO2();
     }
-    , 1000);
-  };
+  }, [predictedCO2, totalDistance]);
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     calculateDistance();
   };
-
-  // const fetchHotelData = async (page = 1) => {
-  //   try {
-  //     const response = await axios.get(
-  //       `http://localhost:5000/api/hotels?page=${page}`
-  //     );
-  //     const hotels = response.data;
-  //     setHotelData(hotels);
-  //   } catch (error) {
-  //     console.error("Error fetching hotel data:", error);
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchHotelData();
-  // }, []);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -266,48 +199,12 @@ export default function Plan() {
     <div className="travelPlanPage">
       <div className="subNavbar">
         <div className="subNavbar-group">
-          <label htmlFor="arrival">From</label>
-          <input
-            type="text"
-            id="arrival"
-            name="arrival"
-            value={arrival}
-            onChange={handleInputChange}
-          />
-          {arrivalSuggestions.length > 0 && (
-            <div className="autocomplete-dropdown-container  c1">
-              {arrivalSuggestions.map((suggestion, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleSelect(suggestion, "arrival")}
-                >
-                  {suggestion}
-                </div>
-              ))}
-            </div>
-          )}
+          <p>From</p>
+          <Header setCoordinates={setArrivalCoordinates} />
         </div>
         <div className="subNavbar-group">
-          <label htmlFor="destination">To</label>
-          <input
-            type="text"
-            id="destination"
-            name="destination"
-            value={destination}
-            onChange={handleInputChange}
-          />
-          {destinationSuggestions.length > 0 && (
-            <div className="autocomplete-dropdown-container">
-              {destinationSuggestions.map((suggestion, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleSelect(suggestion, "destination")}
-                >
-                  {suggestion}
-                </div>
-              ))}
-            </div>
-          )}
+          <p>To</p>
+          <Header setCoordinates={setDestinationCoordinates} />
         </div>
         <button type="submit" onClick={handleSearchSubmit}>
           <svg
@@ -322,9 +219,6 @@ export default function Plan() {
           </svg>
         </button>
       </div>
-      <Header
-        setCoordinates={setCoordinates}
-      />
 
       <div>
         <h4>Hotels/Attractions</h4>
@@ -345,16 +239,6 @@ export default function Plan() {
           coordinates={coordinates}
           places={places}
         />
-        {/* filtering using rating*/}
-        {/* <FormControl className={classes.formControl}>
-                    <InputLabel>Rating</InputLabel>
-                    <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-                    <MenuItem value="">All</MenuItem>
-                    <MenuItem value="3">Above 3.0</MenuItem>
-                    <MenuItem value="4">Above 4.0</MenuItem>
-                    <MenuItem value="4.5">Above 4.5</MenuItem>
-                    </Select>
-                </FormControl> */}
 
         {places?.map((place, i) => (
           <div key={i}>
@@ -384,31 +268,7 @@ export default function Plan() {
           </div>
         ))}
       </div>
-      {/* <div className="hotelData">
-          {hotelData.map((hotel) => (
-            <div className="hotelCard" key={hotel.id}>
-              <img
-                src={
-                  hotel.medium_url
-                    ? hotel.medium_url
-                    : "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg"
-                }
-                alt={hotel.name}
-                onError={(e) => {
-                  e.target.onerror = null;
-                  e.target.src =
-                    "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg";
-                }}
-              />
-              <div className="hotelInfo">
-                <h3>{hotel.name}</h3>
-                <p>{hotel.summary}</p>
-                <p>{hotel.city}</p>
-                <p>Price: {hotel.price}</p>
-              </div>
-            </div>
-          ))}
-        </div> */}
+
       <div className="card-container">
         <div className="card">
           <h2>Plan Your Trip</h2>
