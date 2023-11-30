510416726133e0d5253fba0dc1f330241ff577a1
suvigyagarg <suvigya.2003.garg@gmail.com>
2023-11-30 23:39:59 +0530

    travel changes

--- client/src/components/discover/discover.jsx
+++ client/src/components/discover/discover.jsx
@@ -5,7 +5,10 @@ import place1 from "./images/place1.jpg";
 import place2 from "./images/place2.jpg";
 import place3 from "./images/place3.jpg";
 import place4 from "./images/place4.jpg";
-
+import place5 from "./images/place5.jpg";
+import place6 from "./images/place6.jpg";
+import place7 from "./images/place7.jpg";
+import place8 from "./images/place8.jpg";
 
 const Discover = () => {
   const hotels = [
@@ -33,7 +36,34 @@ const Discover = () => {
       DestiLong : 76.3594,
       image: place4,
     },
+    {
+      name:"Rishikesh ,Uttarakhand",
+      DestiLat :30.086928,
+      DestiLong :78.267612,
+      image:place5,
 
+    },
+    {
+      name: "Amritsar, Punjab",
+      DestiLat :31.633979,
+      DestiLong :74.872264,
+      image:place6,
+
+    },
+    {
+      name:"Palolem beach, Goa",
+      DestiLat :15.01000,
+      DestiLong :74.02300,
+      image:place7,
+
+    },
+    {
+      name:"Kolkata, West Bengal",
+      DestiLat :22.5626,
+      DestiLong :88.3630,
+      image:place8,
+
+    },
   ]
 
   return (
@@ -44,7 +74,6 @@ const Discover = () => {
           Travel with a Purpose. Experience Responsible Tourism.
         </span>
       </div>
-      <span className="discoverOptionName">Accomodation</span>
       <div className="hotel-list">
         {hotels.map((hotel, index) => (
           <Place
--- client/src/components/discover/hotel.jsx
+++ client/src/components/discover/hotel.jsx
@@ -21,7 +21,9 @@ const Place = ({ name, DestiLat, DestiLong, image }) => {
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
 </svg> {name}</h3>
         <button className="hotel-button" onClick={handleClick}>
-          See More
+          Explore <svg  xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16" >
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+</svg>
          </button>
       </div>
 
